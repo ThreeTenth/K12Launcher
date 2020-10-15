1f6918f871a7b320656a4a35eea880a5eb7f26ca
excing <courage.exc@gmail.com>
2020-10-15 22:43:53 +0800

    update FluidSimulationView

--- source/android/app/src/main/java/xyz/whoam/k12/launcher/FluidSimulationView.kt
+++ source/android/app/src/main/java/xyz/whoam/k12/launcher/FluidSimulationView.kt
@@ -1107,13 +1107,25 @@ class FluidSimulationView(context: Context?, attrs: AttributeSet?) : GLSurfaceVi
             GLES30.glDisable(GLES30.GL_BLEND)
 
             curlProgram.bind()
-            curlProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
+            curlProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
             curlProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, velocity.read.attach(0)) }
             blit(curl)
 
             vorticityProgram.bind()
-            vorticityProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
-            vorticityProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, velocity.read.attach(0)) }
+            vorticityProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
+            vorticityProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(
+                it, velocity.read.attach(
+                    0
+                )
+            ) }
             vorticityProgram.uniforms["uCurl"]?.let { GLES30.glUniform1i(it, curl.attach(0)) }
             vorticityProgram.uniforms["curl"]?.let { GLES30.glUniform1f(it, config[CURL] as Float) }
             vorticityProgram.uniforms["dt"]?.let { GLES30.glUniform1f(it, dt.toFloat()) }
@@ -1121,49 +1133,109 @@ class FluidSimulationView(context: Context?, attrs: AttributeSet?) : GLSurfaceVi
             velocity.swap()
 
             divergenceProgram.bind()
-            divergenceProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
-            divergenceProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, velocity.read.attach(0)) }
+            divergenceProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
+            divergenceProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(
+                it, velocity.read.attach(
+                    0
+                )
+            ) }
             blit(divergence)
 
             clearProgram.bind()
-            clearProgram.uniforms["texelSize"]?.let { GLES30.glUniform1i(it, pressure.read.attach(0)) }
-            clearProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1f(it, config[PRESSURE] as Float  ) }
+            clearProgram.uniforms["texelSize"]?.let { GLES30.glUniform1i(
+                it,
+                pressure.read.attach(0)
+            ) }
+            clearProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1f(
+                it,
+                config[PRESSURE] as Float
+            ) }
             blit(pressure.write)
             pressure.swap()
 
             pressureProgram.bind()
-            pressureProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
-            pressureProgram.uniforms["uDivergence"]?.let { GLES30.glUniform1i(it, divergence.attach(0)) }
+            pressureProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
+            pressureProgram.uniforms["uDivergence"]?.let { GLES30.glUniform1i(
+                it, divergence.attach(
+                    0
+                )
+            ) }
             for (i in 0 until (config[PRESSURE_ITERATIONS] as Int)) {
-                pressureProgram.uniforms["uPressure"]?.let { GLES30.glUniform1i(it, pressure.read.attach(1)) }
+                pressureProgram.uniforms["uPressure"]?.let { GLES30.glUniform1i(
+                    it, pressure.read.attach(
+                        1
+                    )
+                ) }
                 blit(pressure.write)
                 pressure.swap()
             }
 
             gradienSubtractProgram.bind()
-            gradienSubtractProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
-            gradienSubtractProgram.uniforms["uPressure"]?.let { GLES30.glUniform1i(it, pressure.read.attach(0) ) }
-            gradienSubtractProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, pressure.read.attach(1) ) }
+            gradienSubtractProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
+            gradienSubtractProgram.uniforms["uPressure"]?.let { GLES30.glUniform1i(
+                it, pressure.read.attach(
+                    0
+                )
+            ) }
+            gradienSubtractProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(
+                it, pressure.read.attach(
+                    1
+                )
+            ) }
             blit(velocity.write)
             velocity.swap()
 
             advectionProgram.bind()
-            advectionProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
+            advectionProgram.uniforms["texelSize"]?.let { GLES30.glUniform2f(
+                it,
+                velocity.texelSizeX,
+                velocity.texelSizeY
+            ) }
             if (!ext.supportLinearFiltering)
-                advectionProgram.uniforms["dyeTexelSize"]?.let { GLES30.glUniform2f(it, velocity.texelSizeX, velocity.texelSizeY) }
+                advectionProgram.uniforms["dyeTexelSize"]?.let { GLES30.glUniform2f(
+                    it,
+                    velocity.texelSizeX,
+                    velocity.texelSizeY
+                ) }
             val velocityId = velocity.read.attach(0)
             advectionProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, velocityId) }
             advectionProgram.uniforms["uSource"]?.let { GLES30.glUniform1i(it, velocityId) }
             advectionProgram.uniforms["dt"]?.let { GLES30.glUniform1f(it, dt.toFloat()) }
-            advectionProgram.uniforms["dissipation"]?.let { GLES30.glUniform1f(it, config[VELOCITY_DISSIPATION] as Float) }
+            advectionProgram.uniforms["dissipation"]?.let { GLES30.glUniform1f(
+                it,
+                config[VELOCITY_DISSIPATION] as Float
+            ) }
             blit(velocity.write)
             velocity.swap()
 
             if (!ext.supportLinearFiltering)
-                advectionProgram.uniforms["dyeTexelSize"]?.let { GLES30.glUniform2f(it, dye.texelSizeX, dye.texelSizeY) }
-            advectionProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(it, velocity.read.attach(0)) }
+                advectionProgram.uniforms["dyeTexelSize"]?.let { GLES30.glUniform2f(
+                    it,
+                    dye.texelSizeX,
+                    dye.texelSizeY
+                ) }
+            advectionProgram.uniforms["uVelocity"]?.let { GLES30.glUniform1i(
+                it, velocity.read.attach(
+                    0
+                )
+            ) }
             advectionProgram.uniforms["uSource"]?.let { GLES30.glUniform1i(it, dye.read.attach(1)) }
-            advectionProgram.uniforms["dissipation"]?.let { GLES30.glUniform1f(it, config[DENSITY_DISSIPATION] as Float) }
+            advectionProgram.uniforms["dissipation"]?.let { GLES30.glUniform1f(
+                it,
+                config[DENSITY_DISSIPATION] as Float
+            ) }
             blit(dye.write)
             dye.swap()
         }
@@ -1289,11 +1361,13 @@ class FluidSimulationView(context: Context?, attrs: AttributeSet?) : GLSurfaceVi
         }
 
         private fun wrap(value: Double, min: Int, max: Int): Double {
-            TODO("wrap")
+            val range = max - min
+            return if (range == 0) min.toDouble() else (value - min) % range + min
         }
 
         private fun scaleByPixelRatio(input: Int): Int {
-            TODO("scaleByPixelRatio")
+            val pixelRatio = 1.0f
+            return floor(input * pixelRatio).toInt()
         }
 
         private fun getSupportedFormat(
